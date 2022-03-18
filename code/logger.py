63cb8427b3283624fff79308bfafc2356e26842e
Devi1k <570541309@qq.com>
2022-03-18 12:37:40 +0800

    modify logger.py

--- code/logger.py
+++ code/logger.py
@@ -2,12 +2,13 @@ import logging
 
 
 class Logger:
-    def __init__(self):
+    def __init__(self, data_type):
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        self.logger = logging.getLogger("interlist_final")
+        self.type = data_type
+        self.logger = logging.getLogger(self.type + "final")
 
     def getLogger(self):
-        handler1 = logging.FileHandler("base-log.log")
+        handler1 = logging.FileHandler("base-log" + self.type + ".log")
         handler1.setLevel(logging.INFO)
         formatter = logging.Formatter('%(asctime)s|%(name)-12s+ %(levelname)-8s++%(message)s')
         handler1.setFormatter(formatter)
