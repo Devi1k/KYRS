3c9b5e0f15646e0098b57fceb5768929cfbd8da0
Devi1k <570541309@qq.com>
2022-03-18 13:41:54 +0800

    amend user dtype

--- code/user_final.py
+++ code/user_final.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 from logger import Logger
 
-data_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'data', '0315')
-output_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'dataset')
+data_path = os.path.join(os.path.dirname(os.getcwd()), 'data', '0315')
+output_path = os.path.join(os.path.dirname(os.getcwd()), 'dataset')
 if not os.path.exists(data_path):
     os.mkdir(data_path)
 if not os.path.exists(output_path):
@@ -13,9 +13,12 @@ if not os.path.exists(output_path):
 
 log = Logger("user").getLogger()
 
-user = pd.read_csv(os.path.join(data_path, '用户.csv'), sep=',', error_bad_lines=False, encoding='utf-8', header=0)
+user = pd.read_csv(os.path.join(data_path, '用户.csv'), sep=',', encoding='utf-8', header=0)
 df = pd.DataFrame(user, columns=['id', 'sex', 'zan_count'])
-df['id'] = df['id'].astype('int64')
-df['sex'] = df['sex'].astype('int64')
-df['zan_count'] = df['zan_count'].astype('int64')
+df['id'] = pd.to_numeric(df['id']).fillna('0').astype('int64')
+df['sex'] = pd.to_numeric(df['sex']).fillna('0').astype('int64')
+df['zan_count'] = pd.to_numeric(df['zan_count']).fillna('0').astype('int64')
+
+#  = df['sex'].astype('int64')
+# df['zan_count'] = df['zan_count'].astype('int64')
 df.to_csv(os.path.join(output_path, 'user.txt'), sep='\t', encoding='utf-8', index=False)
